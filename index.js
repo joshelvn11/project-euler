b0ac24af54e1e195e916533d86237c70574299cf
JoshBeaver1 <20088401-JoshBeaver1@users.noreply.replit.com>
2023-07-18 15:03:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+function largestPrimeFactor(number) {
+
+  let primeFactors = [];
+  
+  for (let i = 2; i < number; i++){
+   
+    let prime = true;
+    
+    if (number % i === 0){ // Checks if i is a factor of number
+      if (i === 2){
+        primeFactors.push(i);
+        prime = false;
+      } else {
+        for (let n = 0; n < primeFactors.length; n++){
+          if (i % primeFactors[n] === 0){
+            prime = false;
+          }
+        }
+      }
+
+      if (prime === true){
+        primeFactors.push(i);
+      }
+      
+    }
+  }
+
+  //console.log(primeFactors);
+  return primeFactors;
+}
+
+console.log(largestPrimeFactor(60085147));
